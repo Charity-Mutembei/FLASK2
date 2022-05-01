faa215112b8abf0117382b44b5e9eb32c9399be5
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-05-01 15:10:17 +0300

    Doc: API call made

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 #views 
 @app.route('/')
@@ -7,9 +8,16 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    title = 'Home - Catch up with what is around you anywhere'
 
-    return render_template('index.html', title = title)
+       #Getting popular news 
+    popular_news = get_news('popular')
+    print(popular_news)
+
+
+    title = 'Home - Catch up with what is around you anywhere'
+ 
+
+    return render_template('index.html', title = title, popular = popular_news)
 @app.route('/newstoday/<news_id>')
 def news(news_id):
     '''
