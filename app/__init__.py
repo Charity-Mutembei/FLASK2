e32ae2c4b0c5c13401380a493f77737d2adfed22
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-05-01 11:36:15 +0300

    Doc: import of the configurations into the init.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from .config import DevConfig
 
 #initializing application
 app = Flask(__name__, static_url_path='/app/static')
+
+#setting up configuration
+app.config.from_object(DevConfig)
 
 from app import views
