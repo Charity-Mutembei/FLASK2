5bc9806c9a4fd728c9a2b847467624edf3d9d710
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-05-01 12:03:47 +0300

    Doc: Control flows inserted to add logic to the application

--- app/views.py
+++ app/views.py
@@ -7,8 +7,9 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    title = 'Home - Welcome to NewsToday website. Catch up with what is around you anywhere'
 
-    return render_template('index.html')
+    return render_template('index.html', title = title)
 @app.route('/newstoday/<news_id>')
 def news(news_id):
     '''
