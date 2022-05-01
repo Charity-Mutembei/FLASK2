1095b53ceda05adb59207865fa6c24022918f213
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-05-01 11:12:09 +0300

    Feat: Configurations set to fit our needs

---
+++ app/config.py
@@ -0,0 +1,22 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    pass
+
+class ProdConfig (Config):
+    '''
+    Production configuration child class
+    Args:
+        Config: the parent configuration class with general configuration settings
+    '''
+    pass
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    Args: 
+        Config: the parent configuration class with general configuration settings
+    '''
+
+    DEBUG = True
--- app/views.py
+++ app/views.py
@@ -9,3 +9,9 @@ def index():
     '''
 
     return render_template('index.html')
+@app.route('/newstoday/<news_id>')
+def news(news_id):
+    '''
+    View the news page function that returns the news details page and its data
+    '''
+    return render_template('index.html', id = news_id)
